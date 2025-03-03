9b9d73312562ff0cfa6cfc4a6d8799623062a744
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-03-03 20:18:42 +0000

    PROJECTTT

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateTotal() {
+    // Get the quantity of each item, or 0 if no value is entered
+    let pizza = parseFloat(document.getElementById('Pizza').value) || 0;
+    let nachos = parseFloat(document.getElementById('Nachos').value) || 0;
+    let hotdog = parseFloat(document.getElementById('Hotdog').value) || 0;
+    let candyBar = parseFloat(document.getElementById('CandyBar').value) || 0;
+    let popcorn = parseFloat(document.getElementById('Popcorn').value) || 0;
+    let soda = parseFloat(document.getElementById('Soda').value) || 0;
+
+    // Calculate the total
+    let total = (pizza * 2.00) + (nachos * 1.50) + (hotdog * 2.50) + (candyBar * 1.25) + (popcorn * 1.25) + (soda * 2.00);
+       // Update the subtotal element
+       document.getElementById('Subtotal').innerHTML = `Subtotal: $${total.toFixed(2)}`;
+}
+document.querySelectorAll('input[type="number"]').forEach(input => {
+    input.addEventListener('input', calculateTotal);
+});
+function resetButton (){
+    document.getElementById("Subtotal");
+}
